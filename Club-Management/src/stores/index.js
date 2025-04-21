bce873b492c3a55fa907e839f662ea71af198aeb
Fang Zhe <670002424@qq.com>
2025-04-21 19:27:28 +0800

    Delete Club-Management directory

--- Club-Management/src/stores/index.js
+++
@@ -1,8 +0,0 @@
-import { createPinia } from "pinia";
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
-
-// 创建 Pinia 实例并使用持久化插件
-const pinia = createPinia();
-pinia.use(piniaPluginPersistedstate);
-
-export default pinia;
